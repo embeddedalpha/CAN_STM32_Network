7fd478ede34f9648e8c954230f5f0e220c137aff
embeddedalpha <kunalsalvi63@gmail.com>
2024-07-04 23:08:53 -0500

    Adding Filter function

--- CAN_F407/CAN/CAN.c
+++ CAN_F407/CAN/CAN.c
@@ -10,16 +10,32 @@
 
 #include "CAN.h"
 
+/************************************************************************/
 CAN_RX_Typedef RX0[3];
 CAN_RX_Typedef RX1[3];
+extern volatile bool CAN1_RX0_Flag;
+extern volatile bool CAN1_RX1_Flag;
+extern volatile bool CAN2_RX0_Flag;
+extern volatile bool CAN2_RX1_Flag;
+/************************************************************************/
 
-bool CAN1_RX0_Flag;
-
+/************************************************************************/
 void CAN1_RX0_IRQHandler(void) {
-
 	CAN1_RX0_Flag = 1;
-
 }
+/************************************************************************/
+void CAN1_RX1_IRQHandler(void) {
+	CAN1_RX1_Flag = 1;
+}
+/************************************************************************/
+void CAN2_RX0_IRQHandler(void) {
+	CAN2_RX0_Flag = 1;
+}
+/************************************************************************/
+void CAN2_RX1_IRQHandler(void) {
+	CAN2_RX1_Flag = 1;
+}
+
 
 /*
  * @func 			:	CAN_Init
@@ -97,7 +113,7 @@ int CAN_Init(CAN_Config *config)
 	config -> CAN_INSTANCE -> FMR |= 0x1C << 8;
 
     // Enable CAN receive FIFO 0 interrupt
-    CAN1->IER |= CAN_IER_FMPIE0;
+    config -> CAN_INSTANCE->IER |= CAN_IER_FMPIE0;
 
 
 
@@ -105,6 +121,17 @@ int CAN_Init(CAN_Config *config)
 	config -> CAN_INSTANCE->MCR &= ~CAN_MCR_INRQ;
 	config -> CAN_INSTANCE->MCR &= ~CAN_MCR_INRQ;
     while((config -> CAN_INSTANCE->MSR & CAN_MSR_INAK)){}
+
+
+
+
+	return 1;
+}
+
+int CAN_Activate_Callback(CAN_Config *config, uint32_t CAN_ID)
+{
+	config->CAN_INSTANCE->IER = CAN_ID;
+
 
     if(config->CAN_INSTANCE == CAN_Configuration.Instance._CAN1)
     {
@@ -124,10 +151,8 @@ int CAN_Init(CAN_Config *config)
         // Enable CAN1 RX0 interrupt in NVIC
         NVIC_EnableIRQ(CAN2_RX1_IRQn);
     }
-
-
-	return 1;
 }
+
 //
 //int CAN_Filter_Init(CAN_Config *config, CAN_Filter_TypeDef *filter)
 //{
@@ -345,84 +370,81 @@ int CAN_Set_Filter_Mask(CAN_Config *config, uint32_t id, uint32_t mask, uint8_t 
 int CAN_Set_Filter_List(CAN_Config *config,uint32_t id1, uint32_t id2, uint8_t filterBank, uint8_t fifoAssignment)
 {
     // Enter filter initialization mode
-    CAN1->FMR |= CAN_FMR_FINIT;
+    config -> CAN_INSTANCE->FMR |= CAN_FMR_FINIT;
 
     // Deactivate the filter
-    CAN1->FA1R &= ~(1 << filterBank);
+    config -> CAN_INSTANCE->FA1R &= ~(1 << filterBank);
 
     // Set filter scale to 32-bit (each filter bank can store two 32-bit IDs in list mode)
-    CAN1->FS1R |= (1 << filterBank);
+    config -> CAN_INSTANCE->FS1R |= (1 << filterBank);
 
     // Set filter mode to list mode
-    CAN1->FM1R |= (1 << filterBank);
+    config -> CAN_INSTANCE->FM1R |= (1 << filterBank);
 
     // Set the filter IDs
 
     if(id1 < 0x7FF)
     {
-    	 CAN1->sFilterRegister[filterBank].FR1 = id1 << 21;  // Standard ID
+    	 config -> CAN_INSTANCE->sFilterRegister[filterBank].FR1 = id1 << 21;  // Standard ID
     }
     else
     {
-    	CAN1->sFilterRegister[filterBank].FR1 = id1 << 0;  // Extended ID
+    	config -> CAN_INSTANCE->sFilterRegister[filterBank].FR1 = id1 << 0;  // Extended ID
     }
     if(id2 < 0x7FF)
     {
-    	 CAN1->sFilterRegister[filterBank].FR2 = id2 << 21;  // Standard ID
+    	 config -> CAN_INSTANCE->sFilterRegister[filterBank].FR2 = id2 << 21;  // Standard ID
     }
     else
     {
-    	CAN1->sFilterRegister[filterBank].FR2 = id2 << 0;  // Extended ID
+    	config -> CAN_INSTANCE->sFilterRegister[filterBank].FR2 = id2 << 0;  // Extended ID
     }
 
     // Assign the filter to the specified FIFO
     if (fifoAssignment == 0) {
-        CAN1->FFA1R &= ~(1 << filterBank);
+        config -> CAN_INSTANCE->FFA1R &= ~(1 << filterBank);
     } else {
-        CAN1->FFA1R |= (1 << filterBank);
+        config -> CAN_INSTANCE->FFA1R |= (1 << filterBank);
     }
 
     // Activate the filter
-    CAN1->FA1R |= (1 << filterBank);
+    config -> CAN_INSTANCE->FA1R |= (1 << filterBank);
 
     // Exit filter initialization mode
-    CAN1->FMR &= ~CAN_FMR_FINIT;
+    config -> CAN_INSTANCE->FMR &= ~CAN_FMR_FINIT;
 
     return 1;
 }
 
 int CAN_Get_Packet_From_Fifo0(CAN_Config *config, CAN_RX_Typedef *rx)
 {
-
-
-
-
 	while(!CAN1_RX0_Flag){}
 
 
-	if (CAN1->RF0R & CAN_RF0R_FMP0) {
+	if (config -> CAN_INSTANCE->RF0R & CAN_RF0R_FMP0) {
         // Get the ID
-        if (CAN1->sFIFOMailBox[0].RIR & CAN_RI0R_IDE) {
+        if (config -> CAN_INSTANCE->sFIFOMailBox[0].RIR & CAN_RI0R_IDE) {
             // Extended ID
-        	rx->ID = (CAN1->sFIFOMailBox[0].RIR & CAN_RI0R_EXID_Msk) >> CAN_RI0R_EXID_Pos;
+        	rx->ID = (config -> CAN_INSTANCE->sFIFOMailBox[0].RIR & CAN_RI0R_EXID_Msk) >> CAN_RI0R_EXID_Pos;
         } else {
             // Standard ID
-        	rx->ID = (CAN1->sFIFOMailBox[0].RIR & CAN_RI0R_STID_Msk) & CAN_RI0R_STID_Pos;
+        	rx->ID = (config -> CAN_INSTANCE->sFIFOMailBox[0].RIR & CAN_RI0R_STID_Msk) & CAN_RI0R_STID_Pos;
         }
     }
 
 	rx->id_type    = (config->CAN_INSTANCE->sFIFOMailBox[0].RIR & CAN_RI0R_IDE_Msk) >> CAN_RI0R_IDE_Pos;
 	rx->frame_type = (config->CAN_INSTANCE->sFIFOMailBox[0].RIR & CAN_RI0R_RTR_Msk) >> CAN_RI0R_RTR_Pos;
-    rx->data_length = CAN1-> sFIFOMailBox[0].RDTR & CAN_RDT0R_DLC;
+    rx->data_length = config -> CAN_INSTANCE-> sFIFOMailBox[0].RDTR & CAN_RDT0R_DLC;
 
-    rx->data[0] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR);
-    rx->data[1] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR >> 8);
-    rx->data[2] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR >> 16);
-    rx->data[3] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR >> 24);
-    rx->data[4] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR);
-    rx->data[5] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR >> 8);
-    rx->data[6] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR >> 16);
-    rx->data[7] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR >> 24);
+    rx->data[0] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDLR);
+    rx->data[1] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDLR >> 8);
+    rx->data[2] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDLR >> 16);
+    rx->data[3] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDLR >> 24);
+    rx->data[4] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDHR);
+    rx->data[5] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDHR >> 8);
+    rx->data[6] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDHR >> 16);
+    rx->data[7] = (uint8_t)(config -> CAN_INSTANCE->sFIFOMailBox[0].RDHR >> 24);
 
-    CAN1->RF0R |= CAN_RF0R_RFOM0;
+    config -> CAN_INSTANCE->RF0R |= CAN_RF0R_RFOM0;
 }
+
